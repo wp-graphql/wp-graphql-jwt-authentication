29071747293684924dee23ac2c9e63efdbadd7d4
Jason Bahl <jasonbahl@mac.com>
2020-01-28 09:33:06 -0700

    - Make the fields hookable per code review request

--- src/ManageTokens.php
+++ src/ManageTokens.php
@@ -159,9 +159,19 @@ class ManageTokens {
 			],
 		];
 
-		register_graphql_fields( 'RegisterUserInput', $fields );
-		register_graphql_fields( 'CreateUserInput', $fields );
-		register_graphql_fields( 'UpdateUserInput', $fields );
+
+		$mutations_to_add_fields_to = apply_filters('graphql_jwt_auth_add_user_mutation_input_fields', [
+			'RegisterUserInput',
+			'CreateUserInput',
+			'UpdateUserInput',
+		] );
+
+		if ( ! empty( $mutations_to_add_fields_to ) && is_array( $mutations_to_add_fields_to ) ) {
+			foreach ( $mutations_to_add_fields_to as $mutation ) {
+				register_graphql_fields( $mutation, $fields );
+			}
+		}
+
 	}
 
 	/**
