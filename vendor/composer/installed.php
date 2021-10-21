02c476e752945e8bef1f0d8c39ef5a7cbfd000a8
Geoff Taylor <geoff@axistaylor.com>
2021-10-21 19:41:05 -0400

    feat: Adds filter for auth header name

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6f4c18ada3382f51c8aa690aaec3bad994af13f3',
+        'name' => 'wp-graphql/wp-graphql-jwt-authentication',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v4.0.0',
+            'version' => '4.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'reference' => 'dccf163dc8ed7ed6a00afc06c51ee5186a428d35',
+            'dev_requirement' => false,
+        ),
+        'wp-graphql/wp-graphql-jwt-authentication' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6f4c18ada3382f51c8aa690aaec3bad994af13f3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
